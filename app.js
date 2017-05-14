c0cbdd41aebe247b5834f20ad0796abce8058ddf
sutardjo <dodiet@gmail.com>
2017-05-14 01:30:04 -0500

    Added all major functionality. Bug with functions being called repeatedly still exists

--- app.js
+++ app.js
@@ -22,8 +22,24 @@ function deleteItem(itemValue) {
 		}
 	}
 	console.log(itemsInShoppingList);
-	renderShoppingList()
+	renderShoppingList();
 }
+
+// Sets checked value to true
+function checkOff(itemValue) {
+	for (var i = 0; i < itemsInShoppingList.length; i++) {
+		if (itemsInShoppingList[i].itemName === itemValue) {
+			if (itemsInShoppingList[i].checked === false) {
+				itemsInShoppingList[i].checked = true;
+			} else {
+				itemsInShoppingList[i].checked = false;
+			}
+		}
+	}
+	console.log("check has been toggled");
+	renderShoppingList();
+}
+
 
 
 //*************** Render functions ***************
@@ -32,23 +48,18 @@ function deleteItem(itemValue) {
 function renderShoppingList() {
 	$('ul').empty();
 	for (var i = 0; i < itemsInShoppingList.length; i++) {
-		$('ul').append('<li><span class="shopping-item">' + itemsInShoppingList[i].itemName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+		// If item object is not checked
+		if (itemsInShoppingList[i].checked === false) {
+			$('ul').append('<li><span class="shopping-item">' + itemsInShoppingList[i].itemName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+		} else {
+			// If item object is checked
+			$('ul').append('<li><span class="shopping-item shopping-item__checked">' + itemsInShoppingList[i].itemName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+		}
 		whichItemToDelete();
 		clickCheckButton();
 	}
 	
 }
-
-
-
-// Toggle Check
-function checkOff(event) {
-	console.log('checked!');
-	$(event.currentTarget).closest('li').toggleClass('shopping-item__checked');
-	console.log('added check!');
-
-}
-
 
 
 //*************** Event listeners ***************
@@ -67,15 +78,14 @@ function addItem() {
 
 // Listens for Check button click
 function clickCheckButton() {
-	let $checkButton = $('button.shopping-item-toggle');
-	$checkButton.click(function(event) {
-		checkOff(event);
+	$('button.shopping-item-toggle').click(function(event) {
+		itemValue = $(event.currentTarget).closest('li').text().slice(0,-11);
+		checkOff(itemValue);
 	})
 }
 
 // Listens for Delete button click & finds which item to delete
 function whichItemToDelete() {
-	let itemValue;
 	$('button.shopping-item-delete').click(function(event) {
 		itemValue = $(event.currentTarget).closest('li').text().slice(0,-11);
 		deleteItem(itemValue);
